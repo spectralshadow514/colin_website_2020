6c8ff1e71c7f44b0599cca6e27b987b149c4c477
Colin Videlock <colinvidelock@gmail.com>
2020-02-12 14:53:45 -0500

    Initial website push
    Signed-off-by: Colin Videlock <colinvidelock@gmail.com>

---
+++ public_html/public_html/wordpress/wp-content/w3tc-config/master.php
@@ -0,0 +1,9 @@
+<?php exit; ?>{
+    "dbcache.enabled": true,
+    "dbcache.engine": "apc",
+    "objectcache.enabled": true,
+    "objectcache.engine": "apc",
+    "pgcache.enabled": true,
+    "pgcache.engine": "apc",
+    "minify.engine": "apc"
+}
