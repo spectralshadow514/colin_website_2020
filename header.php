9ea14a48cfb7984c7e7f3acded5691cbe434b352
Colin Videlock <colinvidelock@gmail.com>
2020-02-13 11:31:08 -0500

    Minor header update
    Signed-off-by: Colin Videlock <colinvidelock@gmail.com>

--- header.php
+++ header.php
@@ -4,5 +4,16 @@
 </div>
 <ul class="nav">
     <li><a href="https://www.colinvidelock.com/">Home</a></li>
+    <li><a href="/about/">About</a></li>
+    <li><a href="/contact/">Contact</a></li>
+    <li><a href="/portfolio/">Portfolio</a></li>
+    <li class="sub">
+        <a href="#">CMS Examples</a>
+        <ul class="subnav">
+            <li><a href="/magento/">Magento</a></li>
+            <li><a href="/wordpress/">Wordpress</a></li>
+            <li><a href="/joomla/">Joomla</a></li>
+        </ul>
+    </li>
 </ul>
 </header>
