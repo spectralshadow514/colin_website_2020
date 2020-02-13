9f7ba17ad606b62546a8e87cfa054969d10064a4
Colin Videlock <colinvidelock@gmail.com>
2020-02-13 02:13:17 -0500

    Start of header nav
    Signed-off-by: Colin Videlock <colinvidelock@gmail.com>

--- header.php
+++ header.php
@@ -2,4 +2,7 @@
 <div class="logo">  
     <h1><a href="https://www.colinvidelock.com/">Colin Videlock</a></h1>
 </div>
+<ul class="nav">
+    <li><a href="https://www.colinvidelock.com/">Home</a></li>
+</ul>
 </header>
