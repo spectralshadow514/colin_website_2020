3442927768fedb91c2766c574d87c641c505b62f
Colin Videlock <colinvidelock@gmail.com>
2020-02-17 17:25:48 -0500

    Flat File Changes: Edited about page, may still need some minor editing as well as portfolio page needs a bunch of it Signed-off-by: Colin Videlock <colinvidelock@gmail.com>

--- about.php
+++ about.php
@@ -15,49 +15,54 @@ require_once('head.php');
     <div class="container">
         <div class="flex grid-row middle vertical-align">
           <div class="column-12">
-            <h2 class="center">Work and experience</h2>
+            <h2 class="center">Work and Experience</h2>
           </div>
         </div>
         <div class="flex grid-row middle vertical-align">
             <div class="column-4-mid center"><img src="/cv_assets/img/rit-and-sccc-combo.png" alt="Suffolk County Community College and RIT" title="The colleges I went to!" /></div>
             <div class="column-8-mid">
-                <p>I have always been into computers, which helped grow my love of coding when I started in college. I went to Suffolk County Community College from 2008-2010, where I took up courses for programming and art in a program called "Computer Art". It was a good mix of learning how to use a lot of the design programs such as photoshop, while also teaching you the basics of Object Oriented Programming. I had a 3.4 average GPA, made Phi Theeta Kappa, and also made Deans list multiple times while I was there.</p>
-                <p>After Suffolk I went upstate to Rochester Institute of Technology, where I majored in "New Media Interactive Development" which focused on programming while still giving a couple design courses to bolster those skills. Some of my favorite courses included a Free and Open Source Software course, design classes, and the Web Tech courses. While I was there I made deans list, and my senior project, <a href="http://savory.cias.rit.edu/" target="_blank">Savory</a>, also won a <a href="http://www.rit.edu/imagine/sponsor_awards_2014.php" target="_blank">bronze sponsorship award</a>.</p>
+                <p>Born in the '90's I have always been fascinated with electronics, computers, and the technology involved. Since going through college my love for computers and coding has only grown. When I attended Suffolk County Community College from 2008 to 2010, I got involved in courses for programming and art in a program called "Computer Art". It was a good mix of learning how to use many of the design programs such as Photoshop, Indesign, and Illustrator, while also teaching you the basics of Object Oriented Programming. I obtained a 3.4 average GPA, made Phi Theta Kappa, and also made Deans list multiple times.</p>
+                <p>Following Suffolk County Community College I went upstate to Rochester Institute of Technology, where I majored in "New Media Interactive Development" which focused on programming while still taking a few design courses to bolster those skills. Some of my favorite courses included a Free and Open Source Software course, Graphical User Interface Design, and the Web Tech courses. While I was there I made deans list, and my senior project, <a href="http://savory.cias.rit.edu/" target="_blank">Savory</a>, also won a <a href="http://www.rit.edu/imagine/sponsor_awards_2014.php" target="_blank">bronze sponsorship award</a>.</p>
             </div>
         </div>
         <div class="flex grid-row middle vertical-align">
           <div class="column-4-mid center push"><img src="/cv_assets/img/bx3-interactive.png" alt="BX3 Interactive" title="First job at BX3 Interactive" /></div>
           <div class="column-8-mid pull">
-            <p>My first job at BX3 Interactive was the first biggest hurdle I've had as a developer. BX3 was a smaller company, under 10 employees while I was there. I was quickly pushed into many new environments, and learned a lot about how a website is built the hard way. We had an intense designer who was very pixel-perfect in his criticisms. This helped me earn a new understanding in CSS that I had but a loose grasp on before, while also allowing me to get experience with the back and forth between a designer and developer.</p>
-            <p>At BX3 we used a mainly custom CMS that seemed to be based off an early version of joomla, except it hid many of it's bigger content areas in the SQL. This gave me a lot of experience searching and using SQL for displaying and changing content. My main responsibilities at BX3 was building new websites while also learning best practices and being relied on for smaller projects the company had. I ended up leaving to be closer to family back on the east coast, plus I had a better job lined up.</p>
+            <p>The first job I had was at BX3 Interactive which was the biggest hurdle I've had as a developer. BX3 was a smaller company, under 10 employees. I was quickly pushed into many new environments, and learned a lot about how a website is built the hard way. We had an intense designer who was very pixel-perfect in his criticisms. This helped me earn a new understanding in CSS that I had but a loose grasp on before, while also allowing me to get experience with the back and forth between a designer and developer.</p>
+            <p>At BX3 we used a custom CMS that seemed to be based off an early version of joomla, except it hid many of it's bigger content areas in the SQL. This gave me a lot of experience searching and using SQL for displaying and changing content. My main responsibilities at BX3 was building new websites while also learning best practices and being relied on for smaller projects the company had. I ended up leaving to be closer to family back on the east coast, plus I had a better job lined up.</p>
           </div>
         </div>
         <div class="flex grid-row middle vertical-align">
           <div class="column-4-mid center"><img src="/cv_assets/img/d3.svg" alt="D3 Corp" title="Second job at D3 Corp" /></div>
           <div class="column-8-mid">
-            <p>The second job I had was at D3 Corp, a bigger company with a developer team of closer to 15 rather than 2 developers and a team lead. We worked in an agile environment, with morning standups and weekly team meetings talking about where we were at with projects, what issues and problems we ran across, and if there were any new technologies or development news we wished to share. It was a rather close knit team, and we had a lot of good times together. My role there was to aid in others learning of CSS/JS, create websites, make plugins for websites as needed, and some small scripting and program editing/creating. Their technologies were much more advanced than the other places I've worked, using GIT as a code hub and using custom auto-launching programs for websites.</p>
-            <p>At D3 Corp, we used mainly wordpress to create websites which sped things up considerably compared to BX3. Outside of that, their larger clients used either the custom CMS which was pretty well built, or Magento 1.9. Towards the end of my time there we started experimenting using flat file generation using a combination of technologies for smaller websites. When I started looking for more work to expand my knowledge at the company, they couldn't provide me with anything so we ended up splitting amicably.</p>
+            <p>The second job I had was at D3 Corp, a bigger company with a developer team of closer to 15 rather than 2 developers and a team lead. We worked in an agile environment, with morning standups and weekly team meetings discussing where we were at with projects, what issues and problems we ran across, and if there were any new technologies or development news we wished to share. It was a rather close knit team, and we had a lot of good times together. My role there was to help aid in others learning abpit CSS and JS, create websites, make plugins for websites as needed, and some small scripting and program editing/creating. Their technologies were much more advanced than the other places I've worked, using GIT as a code hub and using custom auto-launching programs for websites.</p>
+            <p>At D3 Corp, we used mainly WordPress to create websites which sped things up considerably compared to my experience at BX3. Outside of that, their larger clients used either the custom CMS which was pretty well built, or Magento 1.9. Toward the end of my time there we started experimenting with flat file generation using a combination of technologies for smaller websites. When I started looking for more work to expand my knowledge at the company, they couldn't provide me with anything so we ended up splitting amicably.</p>
           </div>
         </div>
         <div class="flex grid-row middle vertical-align">
           <div class="column-4-mid center push"><img src="/cv_assets/img/techno-goober.png" alt="Techno Goober" title="Last job at Techno Goober" /></div>
           <div class="column-8-mid pull">
-            <p>After leaving D3Corp I found myself quickly getting picked up by Techno Goober. Techno Goober is smaller company with a much more welcoming group of people, some of which have turned into really good friends. As a smaller company the technologies we used weren't as advanced as D3Corps, but utilizing the backups and technology of "Flywheel" ended up saving us from a lot of grief. My role in the company was as the lead developer, answering technical questions for clients and other developers and designers, while being the main person for creation of websites and side projects.</p>
+            <p>After leaving D3Corp I was immediately picked up by Techno Goober. Techno Goober is smaller company with a more welcoming group of people, some of which have turned into really good friends. As a smaller company the technologies we used weren't as advanced as D3Corp, but utilizing the backups and technology of "Flywheel" ended up saving us from a lot of grief. As the lead developer my roles in the company were answering technical questions for clients and other developers and designers, while being the main person for creation of websites and side projects.</p>
           </div>
         </div>
         <div class="flex grid-row middle vertical-align">
           <div class="column-12">
-            <h2 class="center">Outside of work</h2>
+            <hr />
+          </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-12">
+            <h2 class="center">Outside of Work</h2>
           </div>
         </div>
         <div class="flex grid-row middle vertical-align">
           <div class="column-4-mid center"><img src="/cv_assets/img/cv_profile_crop.jpg" class="profile-image" alt="A brown hair, brown eyed man wearing a striped button down shirt" title="Hello! I'm Colin Videlock, welcome to my portfolio." /></div>
           <div class="column-8-mid">
-            <p>I spend a lot of time inside, reading books, playing video games, listening to music, and watching movies&tv.</p>
-            <p>I've built every computer I've owned, currently running an ASRock Z270 motherboard, 32gigs of 3200 ram, an 8700k i7 intel cpu, and an nvidia 1080 gpu. Eventually I plan on watercooling in the next build, but I don't plan on doing that until some time after the next console generation has had a bit of time out in the wild.</p>
-            <p>After many years of collecting I've amassed a substantial list of video games, spanning from the original atari, to the game gear, and all the way up to XBox 1, Switch, and PS4.</p>
-            <p>As a preteen and into my teenage years I played guitar, which really influenced my music tastes. I learned to really appreciate guitarists, my favorite of which being the lead guitarist of Porcupine Tree.</p>
-            <p>I also enjoy exploring and going out to see nature, I've been to a lot of different parks and trails around the country.</p>
+            <p>I spend a lot of time indoors, reading books, playing video games, listening to music, and watching movies and tv.</p>
+            <p>I have built every computer that I have owned. I am currently running an ASRock Z270 motherboard, 32gigs of 3200 ram, an 8700k i7 Intel CPU, and a NVIDIA 1080 GPU. Eventually I plan on watercooling in the next build, but I don't plan on doing that until some time after the next console generation has had a bit of time out in the wild.</p>
+            <p>After many years of collecting I've amassed a substantial list of video games, spanning from the original Atari, to the Game Gear, and all the way up to XBox 1, Switch, and PS4. A gaming pastime of mine is a charity stream called Games Done Quick, a speedrunning event, which I've donated a considerable amount of time and money to their causes.</p>
+            <p>As a preteen and into my teenage years I played guitar, which influenced my music tastes. I learned to appreciate guitarists, my favorite being the lead guitarist of Porcupine Tree.</p>
+            <p>I also enjoy exploring and being in nature, I've been to a lot of different parks and trails around the country.</p>
           </div>
         </div>
     </div>
