5596e1a1261cc83fe38e76ff5ed3bcbdb88aba88
Colin Videlock <colinvidelock@gmail.com>
2020-02-17 00:54:21 -0500

    Flat file site changes: Created and finished the portfolio page (will need editing in the future) Added contact page, will add a form in the future
    Signed-off-by: Colin Videlock <colinvidelock@gmail.com>

---
+++ contact.php
@@ -0,0 +1,69 @@
+<?php
+require_once('head.php');
+?>
+  <?php
+  require_once('header.php');
+  ?>
+  <div class="banner">
+    <div class="banner-image" style="background-image: url('/cv_assets/img/cv_banner_full.jpg');"></div>
+    <div class="overlay-area">
+      <div class="container">
+        <h1>About Me</h1>
+      </div>
+    </div>
+  </div>
+    <div class="container">
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-12">
+            <h2 class="center">Work and experience</h2>
+          </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+            <div class="column-4-mid center"><img src="/cv_assets/img/rit-and-sccc-combo.png" alt="Suffolk County Community College and RIT" title="The colleges I went to!" /></div>
+            <div class="column-8-mid">
+                <p>I have always been into computers, which helped grow my love of coding when I started in college. I went to Suffolk County Community College from 2008-2010, where I took up courses for programming and art in a program called "Computer Art". It was a good mix of learning how to use a lot of the design programs such as photoshop, while also teaching you the basics of Object Oriented Programming. I had a 3.4 average GPA, made Phi Theeta Kappa, and also made Deans list multiple times while I was there.</p>
+                <p>After Suffolk I went upstate to Rochester Institute of Technology, where I majored in "New Media Interactive Development" which focused on programming while still giving a couple design courses to bolster those skills. Some of my favorite courses included a Free and Open Source Software course, design classes, and the Web Tech courses. While I was there I made deans list, and my senior project, <a href="http://savory.cias.rit.edu/" target="_blank">Savory</a>, also won a <a href="http://www.rit.edu/imagine/sponsor_awards_2014.php" target="_blank">bronze sponsorship award</a>.</p>
+            </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-8-mid">
+            <p>My first job at BX3 Interactive was the first biggest hurdle I've had as a developer. BX3 was a smaller company, under 10 employees while I was there. I was quickly pushed into many new environments, and learned a lot about how a website is built the hard way. We had an intense designer who was very pixel-perfect in his criticisms. This helped me earn a new understanding in CSS that I had but a loose grasp on before, while also allowing me to get experience with the back and forth between a designer and developer.</p>
+            <p>At BX3 we used a mainly custom CMS that seemed to be based off an early version of joomla, except it hid many of it's bigger content areas in the SQL. This gave me a lot of experience searching and using SQL for displaying and changing content. My main responsibilities at BX3 was building new websites while also learning best practices and being relied on for smaller projects the company had. I ended up leaving to be closer to family back on the east coast, plus I had a better job lined up.</p>
+          </div>
+          <div class="column-4-mid center"><img src="/cv_assets/img/bx3-interactive.png" alt="BX3 Interactive" title="First job at BX3 Interactive" /></div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-4-mid center"><img src="/cv_assets/img/d3.svg" alt="D3 Corp" title="Second job at D3 Corp" /></div>
+          <div class="column-8-mid">
+            <p>The second job I had was at D3 Corp, a bigger company with a developer team of closer to 15 rather than 2 developers and a team lead. We worked in an agile environment, with morning standups and weekly team meetings talking about where we were at with projects, what issues and problems we ran across, and if there were any new technologies or development news we wished to share. It was a rather close knit team, and we had a lot of good times together. My role there was to aid in others learning of CSS/JS, create websites, make plugins for websites as needed, and some small scripting and program editing/creating. Their technologies were much more advanced than the other places I've worked, using GIT as a code hub and using custom auto-launching programs for websites.</p>
+            <p>At D3 Corp, we used mainly wordpress to create websites which sped things up considerably compared to BX3. Outside of that, their larger clients used either the custom CMS which was pretty well built, or Magento 1.9. Towards the end of my time there we started experimenting using flat file generation using a combination of technologies for smaller websites. When I started looking for more work to expand my knowledge at the company, they couldn't provide me with anything so we ended up splitting amicably.</p>
+          </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-8-mid">
+            <p>After leaving D3Corp I found myself quickly getting picked up by Techno Goober. Techno Goober is smaller company with a much more welcoming group of people, some of which have turned into really good friends. As a smaller company the technologies we used weren't as advanced as D3Corps, but utilizing the backups and technology of "Flywheel" ended up saving us from a lot of grief. My role in the company was as the lead developer, answering technical questions for clients and other developers and designers, while being the main person for creation of websites and side projects.</p>
+          </div>
+          <div class="column-4-mid center"><img src="/cv_assets/img/techno-goober.png" alt="Techno Goober" title="Last job at Techno Goober" /></div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-12">
+            <h2 class="center">Outside of work</h2>
+          </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-4-mid center"><img src="/cv_assets/img/cv_profile_crop.jpg" class="profile-image" alt="A brown hair, brown eyed man wearing a striped button down shirt" title="Hello! I'm Colin Videlock, welcome to my portfolio." /></div>
+          <div class="column-8-mid">
+            <p>I spend a lot of time inside, reading books, playing video games, listening to music, and watching movies&tv.</p>
+            <p>I've built every computer I've owned, currently running an ASRock Z270 motherboard, 32gigs of 3200 ram, an 8700k i7 intel cpu, and an nvidia 1080 gpu. Eventually I plan on watercooling in the next build, but I don't plan on doing that until some time after the next console generation has had a bit of time out in the wild.</p>
+            <p>After many years of collecting I've amassed a substantial list of video games, spanning from the original atari, to the game gear, and all the way up to XBox 1, Switch, and PS4.</p>
+            <p>As a preteen and into my teenage years I played guitar, which really influenced my music tastes. I learned to really appreciate guitarists, my favorite of which being the lead guitarist of Porcupine Tree.</p>
+            <p>I also enjoy exploring and going out to see nature, I've been to a lot of different parks and trails around the country.</p>
+          </div>
+        </div>
+    </div>
+  <?php
+    require_once('footer.php');
+  ?>
+<?php
+require_once('foot.php');
+?>
---
+++ portfolio.php
@@ -0,0 +1,130 @@
+<?php
+require_once('head.php');
+?>
+  <?php
+  require_once('header.php');
+  ?>
+  <div class="banner">
+    <div class="banner-image" style="background-image: url('/cv_assets/img/cv_banner_full.jpg');"></div>
+    <div class="overlay-area">
+      <div class="container">
+        <h1>Projects</h1>
+      </div>
+    </div>
+  </div>
+    <div class="container">
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-12">
+            <h2 class="center"><a href="https://www.rit.edu/" target="_blank">RIT</a></h2>
+          </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+            <div class="column-4-mid center"><a href="http://savory.cias.rit.edu/" target="_blank"><img src="/cv_assets/img/Savory-Waste-Less-Save-More.jpg" alt="The top area of the Savory website" title="Visit the Savory website!" /></a></div>
+            <div class="column-8-mid">
+              <h3 class="center"><a href="http://savory.cias.rit.edu/" target="_blank">Savory</a></h3>
+              <p>My first big web project. It was a collaborative idea to get rid of food waste without much hassle for the end user. The idea was to create an app that knew the contents of your fridge using RFID technology, by utilizing a scanner in your fridge everyday items would be scanned and pushed to a database that would know what you've had in the fridge and for what amount of time it's been in there. Using that data you could know approximate times of expirations to cut down on food waste, while also having the ability to suggest recipes based on what's in there. Also since it was an app, you could check the contents of your fridge while out at the store.</p>
+              <p>My role in the project was building a couple of the web pages which was my first real experience building a website, grabbing data from an api and outputting specialized content. It was a fun and hard project that really opened my eyes to just how powerful a simple idea can be.</p>
+              <p class="center"><a href="http://savory.cias.rit.edu/" target="_blank" class="btn">Visit the site!</a></p>
+            </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-12">
+            <hr />
+          </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-12">
+            <h2 class="center"><a href="https://www.bx3.com/" target="_blank">BX3 Interactive</a></h2>
+          </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-8-mid">
+            <h3 class="center"><a href="https://www.propaneservicesaz.com/" target="_blank">Propane Services LLC</a></h3>
+            <p>My first official website at a company. It was tough, as the work environment was both new and difficult, a lot of work was done through the CMS but some of it had to be done through SQL, which was nerve wracking. The designer was also stricter on pixel perfectness on his designs, so it really helped with learning how margins, padding and borders really worked without the use of bootstrap.</p>
+            <p class="center"><a href="https://www.propaneservicesaz.com/" target="_blank" class="btn">Visit the site!</a></p>
+          </div>
+          <div class="column-4-mid center"><a href="https://www.propaneservicesaz.com/" target="_blank"><img src="/cv_assets/img/phoenix-propane-services-llc.jpg" alt="The top area of the Propane Services LLC website" title="Visit the Propane Services LLC website!" /></a></div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-12">
+            <hr />
+          </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-12">
+            <h2 class="center"><a href="https://d3corp.com/" target="_blank">D3Corp</a></h2>
+          </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-4-mid center"><a href="https://worcestercountyhumanesociety.org/adopt-pets-cats-dogs/" target="_blank"><img src="/cv_assets/img/worcester-county-humane-society.jpg" alt="The top area of the Worcester County Humane Society animal adopt page" title="Visit the Worcester County Humane Society anaiml adopt page!" /></a></div>
+          <div class="column-8-mid">
+            <h3 class="center"><a href="https://worcestercountyhumanesociety.org/adopt-pets-cats-dogs/" target="_blank">Worcester County Humane Society adopt a pet sort and filter</a></h3>
+            <p>Created a sort and filter for pets up for adoption by the Worcester County Humane Society. A mix of JavaScript, JQuery, CSS, GET queries and POST queries to exercise and learn a few things about the best way to do these kind of things.</p>
+            <p class="center"><a class="btn" href="https://worcestercountyhumanesociety.org/adopt-pets-cats-dogs/" target="_blank">Visit the site!</a></P>
+          </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-8-mid">
+            <h3 class="center"><a href="https://splashmountainoc.com/park-info/attractions/" target="_blank">Jolly Roger Park Filter</a></h3>
+            <p>Created a filter, and filled out the attractions, for all the parks on the Jolly Roger sites. This was a larger undertaking as it was a similar, but different, filter per park as each one had different requirements for each ride. It took a bit of enginuity and wordpress knowledge to get it right.</p>
+            <p class="center"><a class="btn" href="https://splashmountainoc.com/park-info/attractions/" target="_blank">Visit the site!</a></P>
+          </div>
+          <div class="column-4-mid center"><a href="https://splashmountainoc.com/park-info/attractions/" target="_blank"><img src="/cv_assets/img/jolly-roger-parks.png" alt="The top area of the Splash Mountain site" title="Visit the Splash Mountain Jolly Roger attractions site!" /></a></div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-12">
+            <hr />
+          </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-12">
+            <h2 class="center"><a href="https://www.technogoober.com/" target="_blank">Techno Goober</a></h2>
+          </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-4-mid center"><a href="https://www.sussexacademy.org/" target="_blank"><img src="/cv_assets/img/sussex-academy.jpg" alt="The top area of the Sussex Academy website" title="Visit the Sussex Academy Website!" /></a></div>
+          <div class="column-8-mid">
+            <h3 class="center"><a href="https://www.sussexacademy.org/" target="_blank">Sussex Academy</a></h3>
+            <p>A fun idea for a college, where we went through and moved all of their documentation over onto the website and needed to keep the theme consistent between pages. Fortunately the inner page styles were simple enough to keep the formats from breaking too much.</p>
+            <p class="center"><a class="btn" href="https://www.sussexacademy.org/" target="_blank">Visit the site!</a></P>
+          </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-8-mid">
+            <h3 class="center"><a href="https://www.immanuelshelter.org/" target="_blank">Immanuel Shelter</a></h3>
+            <p>An initially ambitious rehaul of their admittedly meager website they had won a competition for. There were talks of creating some crazy javascript portions, like donation trackers. Unfortunately we never made it that far, but what we did do has helped them a lot and we had many thanks from them.</p>
+            <p class="center"><a class="btn" href="https://www.immanuelshelter.org/" target="_blank">Visit the site!</a></P>
+          </div>
+          <div class="column-4-mid center"><a href="https://www.immanuelshelter.org/" target="_blank"><img src="/cv_assets/img/immanuel-shelter.jpg" alt="The top portion of the Immanuel Shelter website" title="Visit the Immanuel Shelter Website!" /></a></div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-4-mid center"><a href="https://www.originalhardwood.com/" target="_blank"><img src="/cv_assets/img/pinnacle-flooring.jpg" alt="Top portion of the Pinnacle Flooring Website" title="Visit the Pinnacle Flooring Website!" /></a></div>
+          <div class="column-8-mid">
+            <h3 class="center"><a href="https://www.originalhardwood.com/" target="_blank">Pinnacle Flooring</a></h3>
+            <p>An interesting design combining their idea of moving forward. Lot's of JS and CSS triangles to keep them from stabbing content.</p>
+            <p class="center"><a class="btn" href="https://www.originalhardwood.com/" target="_blank">Visit the site!</a></P>
+          </div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-8-mid">
+            <h3 class="center"><a href="https://web.archive.org/web/20190214141920/http://zzhousedesign.com/" target="_blank">ZZHouse</a></h3>
+            <p>A website designed by the company that contacted us. Every page had a completely different design, but with the same background and colors. It was a long and difficult site, but eventually was launched. Later on they eventually wanted something different and unfortunately decided to go in house with their project.</p>
+            <p class="center"><a class="btn" href="https://web.archive.org/web/20190214141920/http://zzhousedesign.com/" target="_blank">Visit the site!</a></P>
+          </div>
+          <div class="column-4-mid center"><a href="https://web.archive.org/web/20190214141920/http://zzhousedesign.com/" target="_blank"><img src="/cv_assets/img/zz-house.png" alt="The top portion of the ZZHouse website" title="Visit the ZZHouse archive!" /></a></div>
+        </div>
+        <div class="flex grid-row middle vertical-align">
+          <div class="column-4-mid center"><a href="https://web.archive.org/web/20190421210900/https://www.200deep.com/" target="_blank"><img src="/cv_assets/img/200deep.jpg" alt="The top portion of the 200Deep website" title="Visit the 200Deep archive!" /></a></div>
+          <div class="column-8-mid">
+            <h3 class="center"><a href="https://web.archive.org/web/20190421210900/https://www.200deep.com/" target="_blank">200Deep</a></h3>
+            <p>200Deep ended up being a project in turmoil, the owner of the company switched web companies multiple times within 2 years, finishing with using half our design, but using a new company. The design itself was a bit of a crazy angled idea, which caused a lot of issues cross browser. It was a lot of fun to make each section work correctly while also making sure each browser and device could handle it.</p>
+            <p class="center"><a class="btn" href="https://web.archive.org/web/20190421210900/https://www.200deep.com/" target="_blank">Visit the site!</a></P>
+          </div>
+        </div>
+    </div>
+  <?php
+    require_once('footer.php');
+  ?>
+<?php
+require_once('foot.php');
+?>
