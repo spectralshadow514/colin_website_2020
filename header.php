631916a29aa033411a904b1bac2c3d4a9f54b31c
Colin Videlock <colinvidelock@gmail.com>
2020-03-31 23:55:43 -0400

    Removal of extra CMS's and addition of link in header to personal/gaming site Signed-off-by: Colin Videlock <colinvidelock@gmail.com>

--- header.php
+++ header.php
@@ -12,7 +12,7 @@ $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https"
         <ul class="nav">
             <li><a href="<?php echo $actual_link; ?>">Home</a></li>
             <li><a href="/about">About</a></li>
-            <!---<li><a href="/wordpress/">Wordpress</a></li>-->
+            <li><a href="https://spectralshadow.com/">SpectralShadow</a></li>
             <li><a href="/portfolio">Portfolio</a></li>
             <li class="sub">
                 <a href="/contact">Contact</a>
--- wordpress/wp-content/w3tc-config/master.php
+++
@@ -1,9 +0,0 @@
-<?php exit; ?>{
-    "dbcache.enabled": true,
-    "dbcache.engine": "apc",
-    "objectcache.enabled": true,
-    "objectcache.engine": "apc",
-    "pgcache.enabled": true,
-    "pgcache.engine": "apc",
-    "minify.engine": "apc"
-}
